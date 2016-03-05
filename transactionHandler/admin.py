c2b96fe22de188fecab6411459c1114103b1a02b
Steinar <steinarv12@ru.is>
2016-03-05 21:38:43 +0000

    Added the admin menu

--- transactionHandler/admin.py
+++ transactionHandler/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from transactionHandler import models
+
 
 # Register your models here.
+
+admin.site.register(models.Transaction)
+admin.site.register(models.bankInfo)
+admin.site.register(models.userProfile)
